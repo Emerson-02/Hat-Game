c621e1e62df5eff8837bee18bbf6db40ca7ee860
Emerson-02 <69188698+Emerson-02@users.noreply.github.com>
2023-01-10 19:24:31 -0300

    Score System

--- Assets/_Game/Scripts/MyTrigger.cs
+++ Assets/_Game/Scripts/MyTrigger.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class MyTrigger : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D target) {
-        if(target.gameObject.CompareTag("Destroyer"))
+    private GameController gameController;
+
+    private void Start() {
+        gameController = FindObjectOfType<GameController>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D target) //Quando a bola bate no colisor
+    {  
+        if(target.gameObject.CompareTag("Destroyer")) // Quando cair fora do chapéu
         {
+            Destroy(this.gameObject);
+        }
+        else if(target.gameObject.CompareTag("Point")) // Quando cai dentro do chapéu
+        {
+            gameController.score++;
             Destroy(this.gameObject);
         }
     }
